4f12be6bd4c17a3584595fa16056e456f4105edd
Cesar Contreras <cesar.contrerasm@outlook.com>
2017-03-26 14:08:54 -0400

    Adds Maps Kit functionality with location updates

--- SwiftMaps/FirstViewController.swift
+++ SwiftMaps/FirstViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.showsUserLocation = true
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,8 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView.centerCoordinate = userLocation.location!.coordinate
+    }
 }
 
